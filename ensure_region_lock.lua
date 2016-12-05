059340d7a62e32c9fc85838d504a60319d9e1e6d
Lukas Pirl <git@lukas-pirl.de>
2016-12-05 14:15:02 +0100

    added script to ensure all selected regions are locked

---
+++ ensure_region_lock.lua
@@ -0,0 +1,12 @@
+ardour {["type"] = "EditorAction",
+        name        = "ensure region lock",
+	license     = "GPLv3",
+	author      = "Lukas Pirl",
+	description = [[ensure selected regions are locked]]
+}
+
+function factory () return function ()
+	for r in Editor:get_selection().regions:regionlist():iter() do
+		r:set_locked(true)
+	end
+end end
